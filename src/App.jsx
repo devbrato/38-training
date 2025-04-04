f0d03d99ea164393ea0ee42f5fed0c3d6350f80b
devbrato <134305737+devbrato@users.noreply.github.com>
2025-04-04 23:34:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import "./App.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Header from "./Component/Header/header.jsx";  // Added .jsx
-import Login from "./Component/Login/login.jsx";    // Added .jsx
-import Category from "./Component/CategoreyScreen/Categorey.jsx";  // Added .jsx
-import Footer from "./Component/Footer/Footer.jsx";  // Added .jsx
+ import Login from "./Component/Login/login.jsx";    // Added .jsx
+ import Category from "./Component/CategoreyScreen/Categorey.jsx";  // Added .jsx
+ import Footer from "./Component/Footer/Footer.jsx";  // Added .jsx
 
 const categories = {
   service: [
